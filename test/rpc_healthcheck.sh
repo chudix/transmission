e65c768a7c221d24220480c2a6bb4095e260d849
chudix <stkboy@gmail.com>
2020-05-19 21:43:24 -0300

    Add transmission RPC healthcheck script
    Needed to determine if container's transmission service is up and
    running

---
+++ test/rpc_healthcheck.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if transmission rpc is running
+# It check 5 times before giving up waiting 2 seconds
+# in between.
+
+echo "Checking transmission RPC"
+OUTPUT=1
+retries="5"
+
+while [ $retries -gt 0 -a $OUTPUT -ne 0 ]
+do
+    echo "Trying to reach transmission rpc"
+    transmission-remote > /dev/null 2>&1
+    OUTPUT=$?
+    if [ $OUTPUT -ne 0 ]
+    then
+        echo "Transmission not responding. Retrying..."
+        sleep 2
+    fi
+    retries=$[$retries-1]
+done
+
+if [ $OUTPUT -eq 0 ]
+then
+    echo "Transmission responded succesfully"
+    exit 0
+else
+    echo "Transmission not responding"
+    exit 127
+fi
